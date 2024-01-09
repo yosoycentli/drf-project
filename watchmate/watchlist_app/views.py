44f26dd18cc0ce2991ede6881580812fc937f2a8
Centli Allan Garcés Buendia <yosoycentli@gmail.com>
2024-01-09 15:25:58 -0600

    [CREATE] Serializers - POST, PUT Request

--- watchmate/watchlist_app/views.py
+++ watchmate/watchlist_app/views.py
@@ -1,22 +1,22 @@
-from django.shortcuts import render
-from watchlist_app.models import Movie
-from django.http import JsonResponse
+# from django.shortcuts import render
+# from watchlist_app.models import Movie
+# from django.http import JsonResponse
 
-# Create your views here.
-def movie_list(request):
-    movies = Movie.objects.all()
-    values = movies.values()
+# # Create your views here.
+# def movie_list(request):
+#     movies = Movie.objects.all()
+#     values = movies.values()
 
-    data = {
-        'movies': list(values)
-    }
-    return JsonResponse(data)
+#     data = {
+#         'movies': list(values)
+#     }
+#     return JsonResponse(data)
 
-def movie_details(request, pk):
-    movie = Movie.objects.get(pk=pk)
-    data = {
-        'name': movie.name,
-        'description': movie.description,
-        'active': movie.active
-    }
-    return JsonResponse(data)
+# def movie_details(request, pk):
+#     movie = Movie.objects.get(pk=pk)
+#     data = {
+#         'name': movie.name,
+#         'description': movie.description,
+#         'active': movie.active
+#     }
+#     return JsonResponse(data)
