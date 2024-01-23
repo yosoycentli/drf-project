5bb0dc869a588cc4bce8b2a449692ddbfe27c368
Centli Allan Garcés Buendia <yosoycentli@gmail.com>
2024-01-23 17:41:44 -0600

    [ADD] Token Authentication - Part 6 (Logout) - If we logout, our token will be deleted, so we cannot access to anything

--- watchmate/user_app/api/views.py
+++ watchmate/user_app/api/views.py
@@ -1,10 +1,18 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
+from rest_framework import status
 
 from user_app.api.serializers import RegistrationSerializer
 from user_app import models
 
+
+@api_view(['POST',])
+def logout_view(request):
+
+    if request.method == 'POST':
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK)
 
 @api_view(['POST',])
 def registration_view(request):
