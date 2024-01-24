4e0714e839c2c4e377ec0afb76ebb58db8efc9e5
Centli Allan Garcés Buendia <yosoycentli@gmail.com>
2024-01-24 16:42:05 -0600

    [MODIFY] JWT Authentication - Login

--- watchmate/watchmate/settings.py
+++ watchmate/watchmate/settings.py
@@ -135,6 +135,11 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         # 'rest_framework.authentication.BasicAuthentication',
         # 'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
+        # 'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
+}
+
+SIMPLE_JWT = {
+    'ROTATE_REFRESH_TOKENS':True,
 }
