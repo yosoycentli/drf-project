28f9af5a7921ed5b342be8ae0ab38d0616065baa
Centli Allan Garcés Buendia <yosoycentli@gmail.com>
2024-01-24 17:29:39 -0600

    [ADD] JWT Authentication - Registration

--- watchmate/watchmate/settings.py
+++ watchmate/watchmate/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -142,4 +143,6 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'ROTATE_REFRESH_TOKENS':True,
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=14),
 }
