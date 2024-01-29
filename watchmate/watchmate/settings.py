e4a97ea2c9b6b79e433c61d86113984653622f0c
Centli Allan Garcés Buendia <yosoycentli@gmail.com>
2024-01-29 12:27:19 -0600

    [ADD] Pagination - Global settings style

--- watchmate/watchmate/settings.py
+++ watchmate/watchmate/settings.py
@@ -150,7 +150,9 @@ REST_FRAMEWORK = {
         'review-create': '20/day',
         'review-list': '10/day',
         'review-detail': '2/day',
-    }
+    },
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 5
 }
 
 # SIMPLE_JWT = {
