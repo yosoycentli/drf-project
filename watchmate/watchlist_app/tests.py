b4b9b5ac6c160daf2f0558fdbd278ac11d7bb334
Centli Allan Garcés Buendia <yosoycentli@gmail.com>
2024-01-29 22:38:12 -0600

    [ADD] API Testing - StreamPlatform

--- watchmate/watchlist_app/tests.py
+++ watchmate/watchlist_app/tests.py
@@ -1,3 +1,39 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your tests here.
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework.authtoken.models import Token
+
+from watchlist_app.api import serializers
+from watchlist_app import models
+
+class StreamPlatfromTestCase(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='example', 
+                                             password="NewPasswor@123")
+        self.token = Token.objects.get(user__username=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+
+        self.stream = models.StreamPlatform.objects.create(name="Netflix", 
+                                                           about="#1 Platfrom", 
+                                                           website="https://www.netflix.com")
+
+    def test_streamplatform_create(self):
+        data = {
+            "name": "Netflix",
+            "about": "#1 Streaming Platform",
+            "website": "https://www.netflix.com"
+        }
+
+        response = self.client.post(reverse('streamplatform-list'), data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_streamplatform_list(self):
+        response = self.client.get(reverse('streamplatform-list'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
+    def test_streamplatform_ind(self):
+        response = self.client.get(reverse('streamplatform-detail', args=(self.stream.id,)))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
