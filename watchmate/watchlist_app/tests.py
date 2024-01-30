1dbb94798eddb23d0ad2c2e0a7beae97d99e5992
Centli Allan Garcés Buendia <yosoycentli@gmail.com>
2024-01-30 15:40:08 -0600

    [ADD] API Testing - WatchList

--- watchmate/watchlist_app/tests.py
+++ watchmate/watchlist_app/tests.py
@@ -29,6 +29,15 @@ class StreamPlatfromTestCase(APITestCase):
 
         response = self.client.post(reverse('streamplatform-list'), data)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+    
+    def test_watchlist_delete(self):
+        data = {
+            "name": "Netflix",
+            "about": "#1 Streaming Platform",
+            "website": "https://www.netflix.com"
+        }
+        response = self.client.delete(reverse('streamplatform-list'), data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_streamplatform_list(self):
         response = self.client.get(reverse('streamplatform-list'))
@@ -37,3 +46,40 @@ class StreamPlatfromTestCase(APITestCase):
     def test_streamplatform_ind(self):
         response = self.client.get(reverse('streamplatform-detail', args=(self.stream.id,)))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+class WatchListTestCase(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='example', 
+                                             password="NewPasswor@123")
+        self.token = Token.objects.get(user__username=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+
+        self.stream = models.StreamPlatform.objects.create(name="Netflix", 
+                                                           about="#1 Platfrom", 
+                                                           website="https://www.netflix.com")
+        self.watchlist = models.WatchList.objects.create(platform=self.stream, 
+                                                         title="Example Movie",
+                                                         storyline="Example Story",
+                                                         active=True)
+
+
+    def test_watchlist_create(self):
+        data = {
+            "platform": self.stream,
+            "title": "Example Movie",
+            "storyline": "Example Story",
+            "active": True
+        }
+        response = self.client.post(reverse('watchlist-list'), data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_watchlist_list(self):
+        response = self.client.get(reverse('watchlist-list'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_watchlist_ind(self):
+        response = self.client.get(reverse('watchlist-detail', args=(self.watchlist.id,)))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(models.WatchList.objects.count(), 1)
+        self.assertEqual(models.WatchList.objects.get().title, 'Example Movie')
